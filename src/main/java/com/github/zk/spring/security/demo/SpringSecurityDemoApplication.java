4a2dcafb8a5abbb98872b71d469184a4aa2cfc49
zhaokai <506683059@qq.com>
2021-06-30 11:30:03 +0800

    增加MyBatisPlus支持

--- src/main/java/com/github/zk/spring/security/demo/SpringSecurityDemoApplication.java
+++ src/main/java/com/github/zk/spring/security/demo/SpringSecurityDemoApplication.java
@@ -1,9 +1,11 @@
 package com.github.zk.spring.security.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.github.zk.spring.security.demo.dao")
 public class SpringSecurityDemoApplication {
 
     public static void main(String[] args) {
